2f8941aa65636965d1fcb489fe0e42b873cb2639
baesomi <smbae0249@gmail.com>
2017-03-23 19:48:57 -0700

    sample server

---
+++ server.js
@@ -0,0 +1,78 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+app.use(function (req, res, next) {
+  console.log('Time:', Date.now());
+  next();
+})
+
+app.use(express.static('public'));
+
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+});
+
+app.post('/user', function (req, res) {
+
+ console.log('데이터 확인', req.body);
+
+  // TODO 실제로 DB 데이터를 저장하는 로직을 개발해야 함.
+
+  res.send({state: 'OK', data: req.body});
+
+ // res.send('POST (Create) ');
+
+});
+
+app.get('/user/:userId', function (req, res) {
+
+  console.log(req.params.userId + '의 정보를 가져옵니다');
+
+  // TODO 실제로 DB 에서 userId 에 해당하는 사용자 정보를 가져오는 로직을 개발해야 함
+  var user = {
+    userId: 13579,
+    name: 'John',
+    email: 'yohany_AT_gmail.com',
+    company: 'KossLAB'
+  };
+
+  res.send(user);
+
+ // res.send('GET (Read) ');
+});
+
+app.put('/user/:userId', function (req, res) {
+  res.send('PUT (Update) ');
+});
+
+app.delete('/user/:userId', function (req, res) {
+  res.send('DELETE (Delete) ');
+});
+
+
+
+app.get('/user/search', function (req, res) {
+
+  console.log('데이터 확인', req.query.name);
+
+  // TODO 실제로 DB 데이터를 조회하는 로직을 개발해야 함.
+
+  var users = [{
+    userId: 13579,
+    name: 'John',
+    email: 'yohany_AT_gmail.com',
+    company: 'KossLAB'
+  }];
+
+  res.send({result: users});
+
+});
